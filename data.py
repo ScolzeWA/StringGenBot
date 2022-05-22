c125ee569aa07c0cd66036873b59f5a7249a54f2
ScolzeWA <64407840+ScolzeWA@users.noreply.github.com>
2022-05-22 15:18:29 +0200

    Add files via upload

---
+++ data.py
@@ -0,0 +1,23 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    generate_single_button = [InlineKeyboardButton("🙄 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🙄", callback_data="generate")]
+
+    generate_button = [generate_single_button]
+
+    buttons = [
+        generate_single_button,
+        [InlineKeyboardButton("❣️ sᴜᴩᴩᴏʀᴛ ❣️", url="https://t.me/Gr_World_Music"),
+         InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url="https://t.me/Scolze_WA"),
+        ],
+    ]
+
+    START = """
+Hᴇʏ {},
+
+Tʜɪs ɪs {},
+Aɴ ᴏᴩᴇɴ sᴏᴜʀᴄᴇᴅ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
+
+Mᴀᴅᴇ ᴡɪᴛʜ ʙʏ : [𝘿𝘼𝘿 𝙎𝘾𝙊𝙇𝙕𝙀](https://t.me/Scolze_WA) !
+    """
